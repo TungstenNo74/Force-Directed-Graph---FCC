4a2533fdab6aa6bc9085980c5c9587200d98abb3
Christian Wimber <ctwimber+1@gmail.com>
2016-02-11 01:45:41 +0000

    RED: yeah, I broke it.  It is currently using only the authors array to generate nodes, but it still isn't working.  I do think I have the link Array sorted though.

---
+++ helpers.js
@@ -0,0 +1,22 @@
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
+
+function uniq(a) {
+    var seen = {};
+    return a.filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
